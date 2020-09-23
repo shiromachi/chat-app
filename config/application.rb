10581f7b68c6b82845f8b032d21b5122e737b12b
shiromachi <mustard.dreieck@gmail.com>
2020-09-23 18:04:46 +0900

    空のメッセージが送信できないバリデーションの設定

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -24,5 +22,8 @@ module ChatApp
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
