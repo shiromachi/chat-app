052e998def7ce95e31e22e4a29a387e3777793cf
shiromachi <mustard.dreieck@gmail.com>
2020-09-24 23:50:58 +0900

    各モデルのテスト

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_100639) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "massages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id"
     t.bigint "room_id"
@@ -72,8 +72,8 @@ ActiveRecord::Schema.define(version: 2020_09_23_100639) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "messages", "rooms"
-  add_foreign_key "messages", "users"
+  add_foreign_key "massages", "rooms"
+  add_foreign_key "massages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
